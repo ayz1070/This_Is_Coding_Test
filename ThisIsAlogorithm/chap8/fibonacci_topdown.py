a6fe20af827da42fe130e5065db3c40f090f1b0b
Jun <ayz1070@gmail.com>
2023-03-07 20:58:50 +0900

    3/7

---
+++ ThisIsAlogorithm/chap8/fibonacci_topdown.py
@@ -0,0 +1,13 @@
+# 탑다운 방식
+d = [0]*100
+
+def fibo(n):
+    if n == 1 or n ==2:
+        return 1
+    if d[n] != 0:
+        return d[n]
+    d[n] = fibo(n-1) + fibo(n-2)
+    return d[n]
+
+print(fibo(5))
+        
